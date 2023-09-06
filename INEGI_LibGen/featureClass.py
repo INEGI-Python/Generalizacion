f4c7a7b765758f822636e1c8f5616c362bffa9c2
Emmanuel-INEGI-2023 <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2023-09-06 13:45:14 -0600

    Update featureClass.py
     Se coloco el primer metodo de la clase Feature Class llamada LEER

--- INEGI_LibGen/featureClass.py
+++ INEGI_LibGen/featureClass.py
@@ -1,12 +1,13 @@
-import numpy
+import numpy as np
+import pandas as pan
+from arcpy import da,env
 
-def LEE():
-  return {"dato":"SE LEYO"}
-  
-
-def ESCRIBE():
-  return {"dato":"SE ESCRIBIO"}
-  
-
-def CAMBIO():
-  return {"dato":"SE CAMBIO"}
+class FeatureClass:
+    def __init__(self,d):
+        self._gdb = d[0]
+        self._fet = d[1]
+    def LEER(s,t,f="*",p=0,e=[0,1]):
+        def lee():
+            return list(da.SearchCursor(s._fet,f))
+        env.workspace = s._gdb
+        return ((("Formato %s no valido" % t if t!="arreglo" else lee()) if t!= "arreglo_np" else np.asarray(lee(),dtype=np.matrix)) if t!= "data_frame" else pan.DataFrame(lee())) if t!="unico" else lee()[e[0]:e[1]]

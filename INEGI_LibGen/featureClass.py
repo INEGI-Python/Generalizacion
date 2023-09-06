3a070bf438f8bd6157d815b6b749857a7ef45e91
Emmanuel-INEGI-2023 <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2023-09-06 15:24:50 -0600

    Update featureClass.py
    Se quito la clase y los metodos se hicieron funciones

--- INEGI_LibGen/featureClass.py
+++ INEGI_LibGen/featureClass.py
@@ -2,12 +2,8 @@ import numpy as np
 import pandas as pan
 from arcpy import da,env
 
-class FeatureClass:
-    def __init__(self,d):
-        self._gdb = d[0]
-        self._fet = d[1]
-    def LEER(s,t,f="*",p=0,e=[0,1]):
-        def lee():
-            return list(da.SearchCursor(s._fet,f))
-        env.workspace = s._gdb
-        return ((("Formato %s no valido" % t if t!="arreglo" else lee()) if t!= "arreglo_np" else np.asarray(lee(),dtype=np.matrix)) if t!= "data_frame" else pan.DataFrame(lee())) if t!="unico" else lee()[e[0]:e[1]]
+def LEER(d,t,f="*",p=0,e=[0,1]):
+    def lee():
+        return list(da.SearchCursor(d[1],f))
+    env.workspace = d[0]
+    return ((("Formato %s no valido" % t if t!="arreglo" else lee()) if t!= "arreglo_np" else np.asarray(lee(),dtype=np.matrix)) if t!= "data_frame" else pan.DataFrame(lee())) if t!="unico" else lee()[e[0]:e[1]]

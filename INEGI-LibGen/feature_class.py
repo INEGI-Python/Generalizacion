08bb97ba09cf088cb22e2042a5c2acd01cdd198e
Emmanuel-INEGI-2023 <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2023-09-06 01:24:39 -0600

    Create feature_class.py
    Se crearon los metodos de los features class

---
+++ INEGI-LibGen/feature_class.py
@@ -0,0 +1,8 @@
+import numpy
+
+def LEE():
+  return {"result":"Se leyo el feature"}
+def ESCRIBE():
+  return {"result":"Se escribio el feature"}
+def ELIMINA():
+  return {"result":"Se Elimino el feature"}

c4327b5eff622342e7643edb3a6f324964ef50f5
Emmanuel-INEGI-2023 <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2023-09-06 16:33:53 -0600

    Update __init__.py
    clase.metodo

--- lib2gen/__init__.py
+++ lib2gen/__init__.py
@@ -1,8 +1,9 @@
-from .codec import FeatureClass
+from codec import FeatureClass.LEER
 
 __versrion__= '1.0.0'
 
+
 def LEER(t,f="*",p=0,e=[0,1]):
-    return FeatureClass().LEER(t,f,p,e)
+    return FeatureClass.LEER(t,f,p,e)
 
 __all__ = ['LEER']

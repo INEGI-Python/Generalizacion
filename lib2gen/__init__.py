46d74c3185bd6edac2647a9c23fe363dba0e54fc
emmanuel-2023 <143114241+emmanuel-2023@users.noreply.github.com>
2023-09-07 15:19:29 -0600

    Update __init__.py
    casi solo

--- lib2gen/__init__.py
+++ lib2gen/__init__.py
@@ -1,12 +1,14 @@
-import numpy as np
-import pandas as pan
-from arcpy import da,env
-    
-def LEER(s,d,t,f="*",p=0,e=[0,1]):
-    def lee():
-        return list(da.SearchCursor(d[1],f))
-    env.workspace = d[0]
-    return ((("Formato %s no valido" % t if t!="arreglo" else lee()) if t!= "arreglo_np" else np.asarray(lee(),dtype=np.matrix)) if t!= "data_frame" else pan.DataFrame(lee())) if t!="unico" else lee()[e[0]:e[1]]
+import codec as feature
+
+__version__ = "1.0.0"
+
+
+
+    #def LEER(s,d,t,f="*",p=0,e=[0,1]):
+   #     def lee():
+     #       return list(da.SearchCursor(d[1],f))
+   #     env.workspace = d[0]
+   #     return ((("Formato %s no valido" % t if t!="arreglo" else lee()) if t!= "arreglo_np" else np.asarray(lee(),dtype=np.matrix)) if t!= "data_frame" else pan.DataFrame(lee())) if t!="unico" else lee()[e[0]:e[1]]
 
 
 

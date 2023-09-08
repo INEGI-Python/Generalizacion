848ace54895cb052c0a8dcd2c878324f5373d1e1
Emmanuel-INEGI-2023 <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2023-09-08 06:12:24 -0600

    Update __init__.py

--- inegi4u/__init__.py
+++ inegi4u/__init__.py
@@ -3,8 +3,8 @@ from .codigo import LEER,TOPOLOGIA
 __version__ = "1.0.0"
 
 def LEER(d,t,p=0,f="*",e=[0,1]):
-    return codigo.LEER(d,t,p,f,e)
+    return codigo().LEER(d,t,p,f,e)
 def TOPOLOGIA(d,t,p):
-    return codigo.TOPOLOGIA(d,t,p)
+    return codigo().TOPOLOGIA(d,t,p)
 
 __ALL__ = ["LEER","TOPOLOGIA"]

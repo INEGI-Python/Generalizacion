47ffbf8ddac62b0cfced451cc3c1e01894f252b5
Emmanuel-INEGI-2023 <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2023-09-08 06:05:49 -0600

    Update codigo.py
    cambio nombre funciones

--- inegi4u/codigo.py
+++ inegi4u/codigo.py
@@ -1,10 +1,10 @@
 import numpy
 import pandas
 
-def LEER(d,t,p=0,f="*",e=[0,1]):
+def _leer(d,t,p=0,f="*",e=[0,1]):
     lee = lambda x:x*x
     return numpy.array(map(lee,e))
 
-def TOPOLOGIA(d,t,p):
+def _topo(d,t,p):
     topo = lambda x:"Topologia elegida: %s" % x
     return pandas.DataFrame(map(topo,[f for f in dir(pandas)]))

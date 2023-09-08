96d68b902146252073a83d041498bfbc771506b6
Emmanuel-INEGI-2023 <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2023-09-08 06:01:35 -0600

    Update __init__.py
    renombrar librerias en la importacion

--- inegi4u/__init__.py
+++ inegi4u/__init__.py
@@ -1,10 +1,10 @@
-from .codigo import LEER,TOPOLOGIA
+from .codigo import LEER as _leer,TOPOLOGIA as _topo
 
 __version__ = "1.0.0"
 
 def LEER(d,t,p=0,f="*",e=[0,1]):
-    return LEER(d,t,p,f,e)
+    return _leer(d,t,p,f,e)
 def TOPOLOGIA(d,t,p):
-    return TOPOLOGIA(d,t,p)
+    return _topo(d,t,p)
 
 __ALL__ = ["LEER","TOPOLOGIA"]

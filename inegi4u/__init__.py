efd17009ee13e2a4f36714a5f91fa4231da32d8d
Emmanuel-INEGI-2023 <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2023-09-08 11:12:19 -0600

    Update __init__.py
    sin puenter funciones

--- inegi4u/__init__.py
+++ inegi4u/__init__.py
@@ -1,10 +1,4 @@
 from .featClass import LEER,AGRUPAR
 
 __version__ = "1.0.0"
-
-def LEER(d,t,p=0,f="*",e=[0,1]):
-    return featClass().LEER(d,t,p,f,e)
-def AGRUPAR(d,t,f="*",g="@OID"):
-    return featClass().AGRUPAR(d,t,p)
-
 __ALL__ = ["LEER","AGRUPAR"]

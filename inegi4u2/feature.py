96a163d2ed4ceb05ca707144e881f4eb570b7b7b
Emmanuel-INEGI-2023 <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2023-09-11 14:51:54 -0600

    Actualizar feature.py
    Desde elnCelular

--- inegi4u2/feature.py
+++ inegi4u2/feature.py
@@ -1,8 +1,10 @@
 import numpy
-
+import datetime
+obj={status:True,data:[],fecha:datetime.datetime(),user:None}
 def leer():
   return "Aqui va el código que leera un FeatureClass y lo devolvera en un formato especifico. DataFrame, Array_numpy, array"
-def actualizar():
-  return "Codigo donde se actualizac algo."
-def eliminar():
-  return "Aqui poner codigo para eliminmar algo."
+def actualizar(u):
+  obj["user"]=u
+  return obj
+def eliminar(e):
+  return "Aqui poner codigo para eliminmar algo"

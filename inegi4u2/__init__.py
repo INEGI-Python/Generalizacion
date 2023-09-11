3969dcfa8c8a94f36ad6e3cb782214b82053a335
Emmanuel-INEGI-2023 <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2023-09-11 16:26:03 -0600

    Update __init__.py
     Funciones con argumentos

--- inegi4u2/__init__.py
+++ inegi4u2/__init__.py
@@ -4,6 +4,10 @@ __version__="1.0"
 
 def leer():
     return feature().leer()
+def actualizar(_u):
+    return feature().actualizar(_u)
+def eliminar(_u):
+    return feature().eliminar(_u)
                    
 
 

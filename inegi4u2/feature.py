81d19e018c051b278724eab153974c9b566eb826
Emmanuel-INEGI-2023 <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2023-09-11 13:14:47 -0600

    Update feature.py
    se agregaron textos

--- inegi4u2/feature.py
+++ inegi4u2/feature.py
@@ -3,6 +3,6 @@ import numpy
 def leer():
   return "Aqui va el código que leera un FeatureClass y lo devolvera en un formato especifico. DataFrame, Array_numpy, array"
 def actualizar():
-  return "Codigo donde se actualizac algo"
+  return {estatus:True,datos:"Codigo donde se actualizac algo"}
 def eliminar():
-  return "Aqui poner codigo para eliminmar algo.  "
+  return "Aqui poner codigo para eliminmar algo."

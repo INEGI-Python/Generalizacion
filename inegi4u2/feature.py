ccda7a50101ac722c0e0b1a6969679c6f97a2b53
Emmanuel-INEGI-2023 <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2023-09-11 15:23:31 -0600

    Update feature.py
    con clase

--- inegi4u2/feature.py
+++ inegi4u2/feature.py
@@ -1,10 +1,10 @@
 import numpy
 import datetime
-
-def leer():
-    return "Aqui va el código que leera un FeatureClass y lo devolvera en un formato especifico. DataFrame, Array_numpy, array"
-def actualizar(u):
-    obj={status:True,data:[],fecha:datetime.datetime(),user:u}
-    return obj
-def eliminar(e):
-    return numpy.array(54,88,99,3,81,3)
+class feature:
+    def leer():
+        return "Aqui va el código que leera un FeatureClass y lo devolvera en un formato especifico. DataFrame, Array_numpy, array"
+    def actualizar(u):
+        obj={status:True,data:[],fecha:datetime.datetime(),user:u}
+        return obj
+    def eliminar(e):
+        return numpy.array(54,88,99,3,81,3)

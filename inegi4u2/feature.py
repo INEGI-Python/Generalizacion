14eb51231a244619cc83e1fc6359fc4f547b107b
Emmanuel-INEGI-2023 <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2023-09-11 15:17:00 -0600

    Update feature.py
    diferentes salidas

--- inegi4u2/feature.py
+++ inegi4u2/feature.py
@@ -1,10 +1,10 @@
 import numpy
 import datetime
-obj={status:True,data:[],fecha:datetime.datetime(),user:None}
+
 def leer():
     return "Aqui va el código que leera un FeatureClass y lo devolvera en un formato especifico. DataFrame, Array_numpy, array"
 def actualizar(u):
-    obj["user"]=u
+    obj={status:True,data:[],fecha:datetime.datetime(),user:u}
     return obj
 def eliminar(e):
     return numpy.array(54,88,99,3,81,3)

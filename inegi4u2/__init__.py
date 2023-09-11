d22266606f59097eb6e64f158cd6127d46354c9c
Emmanuel-INEGI-2023 <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2023-09-11 16:10:04 -0600

    Update __init__.py
    fucntion  leer en  init

--- inegi4u2/__init__.py
+++ inegi4u2/__init__.py
@@ -2,5 +2,9 @@ from .feature import feature, leer, actualizar, eliminar
 
 __version__="1.0"
 
+def leer():
+    return feature().leer()
+                   
+
 
 __ALL__ = ["leer","actualizar","eliminar"]

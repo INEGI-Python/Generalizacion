937bae63ebbbd0e5dc1aa547e6c34d2bae9d26a9
Emmanuel-INEGI-2023 <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2023-09-12 16:42:31 -0600

    Update feature.py
    funcion leer completa

--- inegi4u2/feature.py
+++ inegi4u2/feature.py
@@ -1,10 +1,18 @@
-import numpy
+import numpy as np
+import pandas as pan
 import datetime
-class feature(object):
-    def leer():
-        return "Aqui va el código que leera un FeatureClass y lo devolvera en un formato especifico. DataFrame, Array_numpy, array"
-    def actualizar(u):
+from arcpy import env,da
+
+class feature:
+    def __init__(self):
+        pass
+    def leer(s,_dat):  
+        print("feature.py",_dat)
+        lee = lambda _dat:list(da.SearchCursor(_dat["d"][1],_dat["f"]))
+        env.workspace = _dat["d"][0]
+        return (("Formato %s no valido" % _dat["t"] if _dat["t"]!="arreglo" else lee(_dat))   if _dat["t"] != "arreglo_np" else np.asarray(lee(_dat),dtype=np.matrix))  if _dat["t"]!="data_frame" else pan.DataFrame(lee(_dat) )  if _dat["t"] != "unico" else lee(_dat)[_dat["e"][0]:_dat["e"][1]]
+    def actualizar(s,u):
         obj={"status":True,"data":[],"fecha":datetime.date.today(),"user":u}
         return obj
-    def eliminar(e):
-        return numpy.array([54,88,99,3,81,e])
+    def eliminar(s,e):
+        return np.array([54,55,25,685,e])

018dc57d511d9a3b3466173dfd0fc8ddd142a1b2
Emmanuel-INEGI-2023 <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2023-09-13 10:34:42 -0600

    Update feature.py
    leer final

--- inegi4u2/feature.py
+++ inegi4u2/feature.py
@@ -7,7 +7,6 @@ class feature:
     def __init__(self):
         pass
     def leer(s,_dat):  
-        print("feature.py",_dat)
         lee = lambda _dat:list(da.SearchCursor(_dat["d"][1],_dat["f"]))
         env.workspace = _dat["d"][0]
         return (("Formato %s no valido" % _dat["t"] if _dat["t"]!="arreglo" else lee(_dat))   if _dat["t"] != "arreglo_np" else np.asarray(lee(_dat),dtype=np.matrix))  if _dat["t"]!="data_frame" else pan.DataFrame(lee(_dat) )  if _dat["t"] != "unico" else lee(_dat)[_dat["e"][0]:_dat["e"][1]]

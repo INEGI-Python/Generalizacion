1b86657a34b11bcbc0757996d056f726afc1285e
Genealizacion (Pruebas) <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2024-09-27 16:36:12 -0600

    Update atajos.py
    texto

--- pyInegi1/atajos.py
+++ pyInegi1/atajos.py
@@ -1,1 +1,9 @@
+def sepa(c="*-*-"):
+    print("%s" * 20  % tuple([c for i in range(20)]))
 
+def imp(text):
+    print("[info]  %s" % str(text))
+    
+GDB="datos/Generalizacion_1.gdb"
+distancia = 0
+data,geometry=[],[]

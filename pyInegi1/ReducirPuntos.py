dd98be3d0df5bd3cfff4f58cdf7032fd890d28ef
Xino <emmanuel.rodriguez@inegi.org.mx>
2024-09-30 07:40:07 -0600

    Por poquito y me lo chingo

--- pyInegi1/ReducirPuntos.py
+++ pyInegi1/ReducirPuntos.py
@@ -1,145 +1,85 @@
-#import numpy as np
 import fiona as fio
 import geopandas as gpd
 import folium as fol
-#import matplotlib.pyplot as  plt
-import atajos as ata
+from atajos import imp,distancia,datos,geometria,GDB,colores
 from time import time as t
 import shapely as sh
 
-ata.distancia=20000
+distancia=20000
 
-def visible(df,v):
-	df['properties']['isvisible']=v
-	ata.data.append(df['properties'])
-	ata.geometry.append(sh.Point(*list(df["geometry"]['coordinates'][0])))
+def polyDist(buf,_i):
+	geometria.append(buf)
+	datos.append({'id':_i})
 
 def algoritmo(dfs,_c):
-	def  datTmp(_obj):
-		df = gpd.GeoDataFrame(data=[_obj['properties']],geometry=[sh.Point(*list(_obj['geometry']['coordinates'][0]))],crs=CRS)
-		print(df)
-		isV = df['isvisible']
-		buf = df.buffer(ata.distancia)
-		vecinos = dfs.sjoin(buf,how="inner",predicate='intersects')
-		print(vecinos)
-		return False
-  		#return  {"id":df['properties']["OBJECTID"],"xy":df['geometry']["coordinates"][0],"hab":df['properties']["num_hab"],"vis":isV,"dist":df['properties']['distancia']} if  isV != 0 else False
-	for i in range(_c-1):
-		p1 = datTmp(dfs[i])
-		if p1:
-			for j in range(i+1,_c):
-				p2 = datTmp(j)				
-				if p2:
-					#print(p2)
-					dist = int(((p2['xy'][0]-p1['xy'][0])**2+(p2['xy'][1]-p1['xy'][1])**2)**0.5)
-					if dist<=ata.distancia:
-						dfs[i]['properties']['distancia']['vecinos'].append({dfs[j]['properties']["OBJECTID"]:dist})
-		visible(dfs[i],0)
-     
-     
-     		# 		
-			# 			#print(dfs)
-			# 			if p1["hab"]>=p2["hab"]:
-			# 				visible(dfs[j],0)							
-			# 			else:
-			# 				visible(dfs[j],1)
-			# 				visible(dfs[i],0)
-			# 				break
-			# visible(dfs[i],1)
-			
-		
-			
+	pass
 		
 import pandas as pd
 t1 = t()
-lys= fio.listlayers(ata.GDB)
+lys= fio.listlayers(GDB)
 print(lys)
-_dataF = gpd.read_file(ata.GDB,layer="BCs_",columns=["OBJECTID","cve_edo","cve_mun","cve_loc","nombre","num_hab"])
-#cant = _dataF.count(numeric_only=True)['OBJECTID']
-data = [{'vecinos':[{i:0}]} for i in _dataF['OBJECTID']]
-_dataF.set_index("OBJECTID")
+_dataF = gpd.read_file(GDB,layer="BCs_",columns=["cve_edo","cve_mun","cve_loc","nombre","num_hab"])
+data = [dict(vecinos=[dict(idx=i,pob=h,visi=True)]) for i,h in zip(_dataF.index,_dataF['num_hab'])]
+#_dataF["idx"]=_dataF.index
 _dataF["gpo"]=0
-_dataF["isvisible"]=-1
+_dataF["isvisible"]=True
 _dataF["distancia"]=pd.DataFrame(data={'data':data})
 CRS = _dataF.crs.to_string() #"EPSG:6372" 
-print(CRS)
-bu = _dataF.buffer((ata.distancia/2)+1)
+imp("Buscando Localidades Cercanas para agruparlas")
+bu = _dataF.buffer((distancia/2)+1)
 buDF = gpd.GeoDataFrame(geometry=bu,crs=CRS)
 uni =bu.union_all()
 uniDF = gpd.GeoDataFrame(data=[{"id":1}],geometry=[uni],crs=CRS)
-#uniDF.to_file("Buffer.shp")
 gpos = uniDF.explode(index_parts=True)
+imp("Generando grupos")
 gpos["id"]=[i[1]+1 for  i in  gpos.index.sort_values().to_list()]
 gpos.set_index("id")
 cont = gpos.count(numeric_only=True)['id']
 agrupados = _dataF.sjoin(gpos,how="inner",predicate='intersects')
 agrupados["gpo"]=agrupados['id']
 agrupados = agrupados.drop(columns=['index_right0','index_right1','id'])
-print("GRUPOS: %d" % cont)
+imp("Total de Grupos generados: %d" % cont)
 porGPO = agrupados.groupby(by="gpo").groups
-oid = agrupados.OBJECTID
 
-#tmp=[agrupados.iloc[porGPO[_o]].__geo_interface__['features'] for _o in porGPO.keys()]
 
 for i in gpos.id:
-	print("GRUPO %d " %i)
 	_cantG=len(porGPO[i])
+	imp("Anlizando grupo %d. Localidades del grupo: %d " % (i,_cantG))
 	if _cantG==1:
-		agrupados.loc[porGPO[i][0],'isvisible'] = 1
+		agrupados.loc[porGPO[i][0],'isvisible'] = True
+		buf = agrupados.loc[porGPO[i][0],'geometry'].buffer(distancia)
+		polyDist(buf,porGPO[i][0])
 	else:
-		_gpo = agrupados.iloc[porGPO[i]]
-		for _g in _gpo.index:
-			_buf=_gpo.loc[_g,'geometry'].buffer(ata.distancia)
-			res = _gpo[_gpo.loc[:,'geometry'].intersects(_buf)]
-			data = [{o:n} for o,n in zip(res['OBJECTID'],res['num_hab'])]
-			pob=_gpo.loc[_g]['num_hab']
-			_d_ = [_dat for _dat in data]
-			for _d in _d_:
-				k=list(_d)[0]
-				_i_V = agrupados.iloc[_d[k]]['isvisible']
-				print(_i_V)
-				if _i_V==-1:				
-					if pob>=_d[k]:
-						agrupados.loc[agrupados['OBJECTID']==k,'isvisible']=1			
-					else:
-						agrupados.loc[agrupados['OBJECTID']==_g,'isvisible']=1
-						agrupados.loc[agrupados['OBJECTID']==k,'isvisible']=0
-						break
-     
-			agrupados.loc[agrupados['OBJECTID']==_g,'isvisible']=0
-   
-   
-			vec = {'vecinos':data}
-			agrupados.loc[_g,'distancia']=vec.values().mapping
+		gpoX = agrupados.iloc[porGPO[i]]
+		for _g in gpoX.index:
+			#imp("Elemento %d " % _g)
+			buf = gpoX.loc[_g,'geometry'].buffer(distancia)			
+			res = gpoX[gpoX.loc[:,'geometry'].intersects(buf)]
+			data = [dict(idx=i,pob=n,visi=v) for i,n,v in zip(res.index,res['num_hab'],res['isvisible'])]			
+			pob=gpoX.loc[_g]['num_hab']
+			vis=gpoX.loc[_g]['isvisible']
+			agrupados.loc[_g,'distancia']={'vecinos':data}.values().mapping
 			
-			#print(res.count(numeric_only=True)['OBJECTID'])
-			ata.geometry.append(_buf)
-			ata.data.append({'id':_g})
-  		#print(_gpo.loc[:,'isvisible'])
-		if False:
-			for loc in _gpo.iterrows():
-				_loc = gpd.GeoDataFrame(geometry=[loc[1].loc['geometry']],crs=CRS)
-				_loc["buffer"] =_loc.buffer(ata.distancia)
-				_loc["within"] =_loc["buffer"].within(_gpo)
-				print(_loc)
-		
-  
+			for a in [aux for aux in data[1:] if aux['visi']==1]:
+				if pob>=a['pob']:
+					agrupados.loc[a['idx'],'isvisible'] = False  
+				else:
+					agrupados.loc[a['idx'],'isvisible'] = True
+					agrupados.loc[_g,'isvisible'] = False
+					break
+   			
+   
 
-	#else:	
-	#	algoritmo(tmp[i],_cant)
-#pVisibles = gpd.GeoDataFrame(data=ata.data,geometry=ata.geometry,crs=CRS)
-f = agrupados.__geo_interface__['features']
-_geoo=[sh.Point(*list(_f['geometry']['coordinates'][0])) for _f in f]
-data = [{'id':o,'dist':d,'isV':v} for o,d,v in zip(agrupados['OBJECTID'],agrupados['distancia'],agrupados['isvisible'])]
-pVisibles = gpd.GeoDataFrame(data=data,geometry=_geoo,crs=CRS)
-
-
+pol = gpd.GeoDataFrame(data=datos,geometry=geometria,crs=CRS)
+pVisibles = gpd.GeoDataFrame(data=agrupados.loc[:,['isvisible','distancia','gpo','num_hab']].to_dict(),geometry=[sh.Point(*list(g['geometry']['coordinates'][0])) for g in agrupados.__geo_interface__['features']],crs=CRS)
 print("Tiempo: %.3f " % float(t()-t1))
+imp("Creando Mapa...")
 
-m = gpos.explore(column="id",cmap="Set1",name="Grupos",legend=False,popup=["id"])
-pVisibles.explore(m=m,column='isV',cmap="Set1",marker_kwds=dict(radius=4, fill=True),legend=False,name="Localidades",popup=["isV"])
+m=pVisibles.explore(column='isvisible',cmap=colores,marker_kwds=dict(radius=3, fill=True),legend=False,name="Localidades",popup=['isvisible','num_hab','gpo'])
+pol.explore(m=m,column="id",color="grey",name="Grupos",legend=False,popup=["id"])
 fol.TileLayer("OpenStreetMap",show=True).add_to(m)
 fol.LayerControl().add_to(m)
+
 
 m.show_in_browser()
 
--- pyInegi1/atajos.py
+++ pyInegi1/atajos.py
@@ -1,9 +1,14 @@
+import datetime as dt
 def sepa(c="*-*-"):
     print("%s" * 20  % tuple([c for i in range(20)]))
 
 def imp(text):
-    print("[info]  %s" % str(text))
-    
+    t = dt.datetime.today()
+    print("[%s]  %s" % (t,str(text)))
+
+def colores(v):
+    return "red" if v==True else "gray"
+
 GDB="datos/Generalizacion.gdb"
 distancia = 0
-data,geometry=[],[]
+datos,geometria=[],[]
